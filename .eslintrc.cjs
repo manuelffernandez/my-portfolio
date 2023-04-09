ac9eebe413ca13426ab15a06db65a27be5641ea1
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-09 16:44:16 -0300

    setup project

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react/jsx-runtime',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {},
+};
