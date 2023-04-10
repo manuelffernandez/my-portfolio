3e9948ebb3be363c10d3563fcd6ba3c160c3ea47
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-10 19:15:47 -0300

    update sections import in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
-import { sections } from '../helpers/sections';
+import { sections } from '../../helpers/sections';
 
 const Header = (): JSX.Element => {
   const bpMedium: number = 968;
