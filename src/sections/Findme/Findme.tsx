3612d48a74634030198db0c3d10ffafcc2422907
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-11 19:37:34 -0300

    integrates Contact in Findme section

--- src/sections/Findme/Findme.tsx
+++ src/sections/Findme/Findme.tsx
@@ -1,13 +1,20 @@
-import { Section } from '../../components';
+import { Contact, Section } from '../../components';
 import { sections } from '../../helpers/sections';
+import { socialNetworkLinks } from '../../helpers/socialNetworkLinks';
+import styles from './Findme.module.scss';
 
 const Findme = (): JSX.Element => {
+  const { contactGrid } = styles;
   return (
     <Section
       sectionName={sections[1]}
       title='Find me'
       subtitle='Get in contact'>
-      <div>Linkedin</div>
+      <div className={contactGrid}>
+        {socialNetworkLinks.map((net, index) => (
+          <Contact key={index} data={net} />
+        ))}
+      </div>
     </Section>
   );
 };
