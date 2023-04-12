3ea1d9f54e260e664c150dacfb3d2877721b7ad5
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-12 16:42:36 -0300

    add toggle segments functionality in Edex comp

--- src/sections/Edex/Edex.tsx
+++ src/sections/Edex/Edex.tsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
 import styles from './Edex.module.scss';
 
 const Edex = (): JSX.Element => {
-  return <div>Edex</div>;
+  const [showEducactionSegment, setShowEducactionSegment] =
+    useState<boolean>(true);
+
+  const {
+    edexCont,
+    edexTab,
+    edexTabButton,
+    edexTabButtonActive,
+    edexSegments,
+    edexSegment,
+  } = styles;
+  return (
+    <div className={edexCont}>
+      <div className={edexTab}>
+        <p
+          className={
+            showEducactionSegment ? edexTabButtonActive : edexTabButton
+          }
+          onClick={() => {
+            setShowEducactionSegment(true);
+          }}>
+          Education
+        </p>
+        <p
+          className={
+            !showEducactionSegment ? edexTabButtonActive : edexTabButton
+          }
+          onClick={() => {
+            setShowEducactionSegment(false);
+          }}>
+          Experiencie
+        </p>
+      </div>
+      <div className={edexSegments}>
+        {showEducactionSegment ? (
+          <div className={edexSegment}>Ed Segment</div>
+        ) : (
+          <div className={edexSegment}>Ed Section</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Edex;
