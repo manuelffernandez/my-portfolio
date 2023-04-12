e278ef1a3faecf89c8eb9a01de7bab0a246c01eb
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-12 15:10:24 -0300

    create edex data

---
+++ src/data/edexData.ts
@@ -0,0 +1,121 @@
+interface Experience {
+  name: string;
+  place: string;
+  description?: string;
+  startDate: Date;
+  finishDate?: Date;
+}
+
+export const edexData: {
+  en: { studies: Experience[]; jobs: Experience[] };
+  es: { studies: Experience[]; jobs: Experience[] };
+} = {
+  en: {
+    studies: [
+      {
+        name: 'Bachelor of Mathematics',
+        place: 'Universidad del Comahue',
+        description:
+          'I did not end the university degree, but I adquired a lot of math knowledges.',
+        startDate: new Date(2018, 2, 1),
+        finishDate: new Date(2020, 10, 30),
+      },
+      {
+        name: 'Web development',
+        place: 'Self-taught',
+        description:
+          'I read tons of documentation and also saw Youtube channels like: FreeCodeCamp, Traversy Media, The Net Ninja, The Coding Train, Midudev.',
+        startDate: new Date(2021, 2, 15),
+      },
+      {
+        name: 'React',
+        place: 'Coderhouse',
+        description:
+          'I began studying more formally through an online institute.',
+        startDate: new Date(2022, 5, 8),
+        finishDate: new Date(2022, 11, 21),
+      },
+      {
+        name: 'PostgreSQL',
+        place: 'EDTeam',
+        description: 'I recently became interested in databases.',
+        startDate: new Date(2022, 3, 5),
+      },
+    ],
+    jobs: [
+      {
+        name: 'Customers service',
+        place: 'Merchandising Teleférico Cerro Otto',
+        startDate: new Date(2016, 0, 1),
+        finishDate: new Date(2017, 11, 20),
+      },
+      {
+        name: 'Bakery entrepreneurship',
+        place: 'my own place',
+        startDate: new Date(2016, 0, 1),
+        finishDate: new Date(2017, 11, 20),
+      },
+      {
+        name: 'Cashier',
+        place: 'Teleférico Cerro Otto',
+        description: 'I gained a lot of team working experience.',
+        startDate: new Date(2022, 0, 3),
+        finishDate: new Date(2022, 11, 31),
+      },
+    ],
+  },
+  es: {
+    studies: [
+      {
+        name: 'Licenciatura en Matemáticas',
+        place: 'Universidad del Comahue',
+        description:
+          'No terminé la carrera, pero adquirí un montón de conociemiento matemáticos.',
+        startDate: new Date(2018, 2, 1),
+        finishDate: new Date(2020, 10, 30),
+      },
+      {
+        name: 'Web development',
+        place: 'Self-taught',
+        description:
+          'Leí documentación y ví canales de Youtube como: FreeCodeCamp, Traversy Media, The Net Ninja, The Coding Train, Midudev.',
+        startDate: new Date(2021, 2, 15),
+      },
+      {
+        name: 'React',
+        place: 'Coderhouse',
+        description:
+          'Comencé a estudiar formalmente a través de un instituo online.',
+        startDate: new Date(2022, 5, 8),
+        finishDate: new Date(2022, 11, 21),
+      },
+      {
+        name: 'PostgreSQL',
+        place: 'EDTeam',
+        description: 'Recientemente me he interesado por las bases de datos.',
+        startDate: new Date(2022, 3, 5),
+      },
+    ],
+    jobs: [
+      {
+        name: 'Atención al cliente',
+        place: 'Merchandising Teleférico Cerro Otto',
+        startDate: new Date(2016, 0, 1),
+        finishDate: new Date(2017, 11, 20),
+      },
+      {
+        name: 'Emprendimiento panadero',
+        place: 'Mi propio lugar',
+        startDate: new Date(2016, 0, 1),
+        finishDate: new Date(2017, 11, 20),
+      },
+      {
+        name: 'Cajero',
+        place: 'Teleférico Cerro Otto',
+        description: 'Gané un monton de experiencia de trabajo en equipo.',
+        startDate: new Date(2022, 0, 3),
+        finishDate: new Date(2022, 11, 31),
+      },
+    ],
+  },
+};
