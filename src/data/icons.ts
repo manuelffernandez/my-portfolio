b2d5ba9f7ffcfac9077d8629c300a4102f09764f
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-13 16:14:11 -0300

    create icons array

---
+++ src/data/icons.ts
@@ -0,0 +1,29 @@
+import htmlIcon from '../assets/icons/html5.svg';
+import cssIcon from '../assets/icons/css3.svg';
+import sassIcon from '../assets/icons/sass.svg';
+import jsIcon from '../assets/icons/javascript.svg';
+import tsIcon from '../assets/icons/typescript.svg';
+import gitIcon from '../assets/icons/git.svg';
+import bsIcon from '../assets/icons/bootstrap.svg';
+import muiIcon from '../assets/icons/mui.svg';
+import eslintIcon from '../assets/icons/eslint.svg';
+import formikIcon from '../assets/icons/formik.svg';
+import reactrouterIcon from '../assets/icons/react-router.svg';
+import reactIcon from '../assets/icons/react.svg';
+import reduxIcon from '../assets/icons/redux.svg';
+
+export const icons = [
+  { title: 'html icon', icon: htmlIcon },
+  { title: 'css 3 icon', icon: cssIcon },
+  { title: 'sass icon', icon: sassIcon },
+  { title: 'javascript icon', icon: jsIcon },
+  { title: 'typescript icon', icon: tsIcon },
+  { title: 'git icon', icon: gitIcon },
+  { title: 'bootstrap icon', icon: bsIcon },
+  { title: 'mui icon', icon: muiIcon },
+  { title: 'eslint icon', icon: eslintIcon },
+  { title: 'formik icon', icon: formikIcon },
+  { title: 'reactrouter icon', icon: reactrouterIcon },
+  { title: 'react icon', icon: reactIcon },
+  { title: 'redux icon', icon: reduxIcon },
+];
