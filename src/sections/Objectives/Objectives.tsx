51ba2f9d92439ea6910319cd0f794f7f3f720063
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-15 11:43:33 -0300

    add Objectives section

---
+++ src/sections/Objectives/Objectives.tsx
@@ -0,0 +1,13 @@
+import styles from './Objectives.module.scss';
+
+const Objectives = (): JSX.Element => {
+  return (
+    <div>
+      {/* <div key={index} className={iconCont}>
+        <img className={iconContImg} src={icon.icon} alt={icon.title} />
+      </div> */}
+    </div>
+  );
+};
+
+export default Objectives;
