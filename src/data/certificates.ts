ec7d2fdc5a3ac1763e24529aebd5e8290a9ad078
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-04-25 21:08:32 -0300

    modify issueDate type and update data

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -1,33 +1,39 @@
 interface Certificate {
   title: string;
   institute: string;
-  issueDate: string;
+  issueDate: Date;
   link: string;
 }
 
 export const certificates: Certificate[] = [
   {
-    title: 'Desarrollo Web',
+    title: 'Web Development',
     institute: 'Coderhouse',
-    issueDate: 'August 2022',
+    issueDate: new Date(2022, 7),
     link: 'https://www.coderhouse.com/certificados/631cea991b5ea30019ab2a4c',
   },
   {
     title: 'JavaScript',
     institute: 'Coderhouse',
-    issueDate: 'October 2022',
+    issueDate: new Date(2022, 9),
     link: 'https://www.coderhouse.com/certificados/6365bdda0fa3c3000f75e7f6',
   },
   {
     title: 'React Js',
     institute: 'Coderhouse',
-    issueDate: 'December 2022',
+    issueDate: new Date(2022, 11),
     link: 'https://www.coderhouse.com/certificados/63b0b31adb7d5f000eb16839',
   },
   {
     title: 'Frontend React',
     institute: 'Coderhouse',
-    issueDate: 'December 2022',
+    issueDate: new Date(2022, 11),
     link: 'https://www.coderhouse.com/certificados/63b0b31adb7d5f000eb1683c',
+  },
+  {
+    title: 'Programming paradigms',
+    institute: 'EDTeam',
+    issueDate: new Date(2023, 3),
+    link: 'https://app.ed.team/u/manuelfernandez1fa/curso/paradigmas',
   },
 ];
