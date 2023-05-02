a573a850d3fc2bd17c59204de8102b0aa0bd37fb
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-05-02 18:43:15 -0300

    update edex data

--- src/data/edexData.ts
+++ src/data/edexData.ts
@@ -8,49 +8,53 @@ interface Experience {
 export const edexData: { studies: Experience[]; jobs: Experience[] } = {
   studies: [
     {
-      title: 'Bachelor of Mathematics',
-      place: 'Universidad del Comahue',
+      title: 'Database administrator',
+      place: 'EDTeam',
       description:
-        'I did not end the university degree, but I acquired a lot of math knowledge.',
-      date: 'March 2018 - Nov. 2020',
+        'Recently became interested in database management systems, so I decided to start the db admin career at EDTeam. Currently I am studying SQL and PostgreSQL.',
+      date: 'Apr. 2023 - Present',
+    },
+    {
+      title: 'React',
+      place: 'Coderhouse',
+      description: 'Began studying more formally through an online institute.',
+      date: 'June 2022 - Dec. 2022',
     },
     {
       title: 'Web development',
       place: 'Self-taught',
       description:
-        'I read tons of documentation and also saw Youtube channels like: Free Code Camp, Traversy Media, The Net Ninja, The Coding Train, Midudev.',
+        'Read tons of documentation and also saw Youtube channels like: Free Code Camp, Traversy Media, The Net Ninja, The Coding Train, Midudev.',
       date: 'March 2021',
     },
     {
-      title: 'React',
-      place: 'Coderhouse',
+      title: 'Bachelor of Mathematics',
+      place: 'Universidad del Comahue',
       description:
-        'I began studying more formally through an online institute.',
-      date: 'June 2022 - Dec. 2022',
-    },
-    {
-      title: 'PostgreSQL',
-      place: 'EDTeam',
-      description: 'I recently became interested in databases.',
-      date: 'Apr. 2023 - Present',
+        'Acquired extensive knowledge in fields like algebra, calculus, geometry, and linear algebra, among others',
+      date: 'March 2018 - Nov. 2020',
     },
   ],
   jobs: [
     {
-      title: 'Customers service',
-      place: 'Merchandising Teleférico Cerro Otto',
-      date: 'Jan. 2016 - Dec. 2017',
+      title: 'HR Collaborator & Cashier',
+      place: 'Teleférico Cerro Otto',
+      description:
+        'Gained a lot of team working experience and communication skills. Collaboration was essential in meeting high demands.',
+      date: 'Jan. 2022 - Dec. 2022',
     },
     {
       title: 'Bakery',
       place: 'My own place',
+      description: 'Acquired management and planification skills.',
       date: 'Dec. 2020 - Oct. 2021',
     },
     {
-      title: 'Cashier',
-      place: 'Teleférico Cerro Otto',
-      description: 'I gained a lot of team working experience.',
-      date: 'Jan. 2022 - Dec. 2022',
+      title: 'Customers service',
+      place: 'Merchandising Teleférico Cerro Otto',
+      description:
+        'Communication, planning and organization were key concepts in my daily work performance."',
+      date: 'Jan. 2016 - Dec. 2017',
     },
   ],
 };
