e0b598e34456779297adff71687145f7e54eaf6c
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-05-01 21:27:21 -0300

    update skills data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,20 +1,3 @@
-import htmlIcon from '../assets/icons/html5.svg';
-import cssIcon from '../assets/icons/css3.svg';
-import sassIcon from '../assets/icons/sass.svg';
-import jsIcon from '../assets/icons/javascript.svg';
-import tsIcon from '../assets/icons/typescript.svg';
-import gitIcon from '../assets/icons/git.svg';
-import bsIcon from '../assets/icons/bootstrap.svg';
-import muiIcon from '../assets/icons/mui.svg';
-import eslintIcon from '../assets/icons/eslint.svg';
-import formikIcon from '../assets/icons/formik.svg';
-import reactrouterIcon from '../assets/icons/react-router.svg';
-import reactIcon from '../assets/icons/react.svg';
-import reduxIcon from '../assets/icons/redux.svg';
-import jestIcon from '../assets/icons/jest.svg';
-import dockerIcon from '../assets/icons/docker.svg';
-import postgresIcon from '../assets/icons/postgresql.svg';
-
 export interface Skill {
   title: string;
   link: string;
@@ -27,112 +10,119 @@ export const skills: Skill[] = [
   {
     title: 'HTML5',
     link: 'https://www.w3.org/',
-    icon: htmlIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985765/portfolio/icons/html5_lu9dap.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'CSS3',
     link: 'https://developer.mozilla.org/es/docs/Web/CSS',
-    icon: cssIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985763/portfolio/icons/css3_lbrylm.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'Sass',
     link: 'https://sass-lang.com/',
-    icon: sassIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985762/portfolio/icons/sass_z17qnd.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'JavaScript',
     link: 'https://developer.mozilla.org/es/docs/Web/JavaScript',
-    icon: jsIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985760/portfolio/icons/javascript_aanw4w.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'TypeScript',
     link: 'https://www.typescriptlang.org/',
-    icon: tsIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985762/portfolio/icons/typescript_cshdp0.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'git',
     link: 'https://git-scm.com/',
-    icon: gitIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985765/portfolio/icons/git_phywxc.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'Bootstrap',
     link: 'https://getbootstrap.com/',
-    icon: bsIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985762/portfolio/icons/bootstrap_yl43eq.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'MaterialUI',
     link: 'https://mui.com/',
-    icon: muiIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985762/portfolio/icons/mui_gkgsdz.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'ESLint',
     link: 'https://eslint.org/',
-    icon: eslintIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985764/portfolio/icons/eslint_onmsie.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'Formik',
     link: 'https://formik.org/',
-    icon: formikIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985764/portfolio/icons/formik_m5ie3h.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'React Router',
     link: 'https://reactrouter.com/en/main',
-    icon: reactrouterIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985761/portfolio/icons/react-router_htbbyt.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'React',
     link: 'https://react.dev/',
-    icon: reactIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985761/portfolio/icons/react_r2asdh.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'Redux',
     link: 'https://redux.js.org/',
-    icon: reduxIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985762/portfolio/icons/redux_xd62ti.svg',
     learned: true,
     studying: false,
   },
   {
     title: 'Jest',
     link: 'https://redux.js.org/',
-    icon: jestIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985760/portfolio/icons/jest_if5rrb.svg',
     learned: false,
     studying: false,
   },
   {
     title: 'Docker',
     link: 'https://redux.js.org/',
-    icon: dockerIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985764/portfolio/icons/docker_tto3vz.svg',
     learned: false,
     studying: false,
   },
   {
     title: 'PostgreSQL',
     link: 'https://redux.js.org/',
-    icon: postgresIcon,
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682985760/portfolio/icons/postgresql_nfwlaf.svg',
+    learned: false,
+    studying: true,
+  },
+  {
+    title: 'Java',
+    link: 'https://redux.js.org/',
+    icon: 'https://res.cloudinary.com/dke7ynizq/image/upload/v1682986365/portfolio/icons/java_cjvj8l.svg',
     learned: false,
     studying: true,
   },
