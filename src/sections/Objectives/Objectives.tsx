0240b2264059d7f893c5afb89daf161ad85c55a7
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-05-02 01:53:56 -0300

    refact section by implementing fetch functions

--- src/sections/Objectives/Objectives.tsx
+++ src/sections/Objectives/Objectives.tsx
@@ -1,48 +1,68 @@
+import { useEffect, useState } from 'react';
+import { fetchSkills } from '../../services/fetchSkills';
 import styles from './Objectives.module.scss';
-import { skills } from '../../data/skills';
+import { type Skill } from '../../interfaces/Skill';
 
 const Objectives = (): JSX.Element => {
   const { objCont, iconCont, iconContTitle, iconContImg, iconContImgDisabled } =
     styles;
 
+  const [objs, setObjs] = useState<Skill[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getObjs = async (): Promise<void> => {
+      const skills = await fetchSkills();
+      setLoading(false);
+      setObjs(skills);
+    };
+
+    setLoading(true);
+    void getObjs();
+  }, []);
+
   return (
     <div className={objCont}>
-      {skills
-        .filter(skill => !skill.learned)
-        .sort((skillA, skillB) => {
-          if (skillA.studying && !skillB.studying) return -1;
-          if (!skillA.studying && skillB.studying) return 1;
-          return 0;
-        })
-        .map((skill, index) => (
-          <div key={index} className={iconCont}>
-            {skill.studying ? (
-              <>
-                <img
-                  className={iconContImg}
-                  src={skill.icon}
-                  alt={skill.title}
-                />
-                <div>
-                  <h5 className={iconContTitle}>{skill.title}</h5>
-                  <p className='color-cyan'>Currently studying</p>
-                </div>
-              </>
-            ) : (
-              <>
-                <img
-                  className={iconContImgDisabled}
-                  src={skill.icon}
-                  alt={skill.title}
-                />
-                <div>
-                  <h5 className={iconContTitle}>{skill.title}</h5>
-                  <p className='color-blue'>Not yet</p>
-                </div>
-              </>
-            )}
-          </div>
-        ))}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        objs
+          .filter(skill => !skill.learned)
+          .sort((skillA, skillB) => {
+            if (skillA.studying && !skillB.studying) return -1;
+            if (!skillA.studying && skillB.studying) return 1;
+            return 0;
+          })
+          .map((skill, index) => (
+            <div key={index} className={iconCont}>
+              {skill.studying ? (
+                <>
+                  <img
+                    className={iconContImg}
+                    src={skill.icon}
+                    alt={skill.title}
+                  />
+                  <div>
+                    <h5 className={iconContTitle}>{skill.title}</h5>
+                    <p className='color-cyan'>Currently studying</p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <img
+                    className={iconContImgDisabled}
+                    src={skill.icon}
+                    alt={skill.title}
+                  />
+                  <div>
+                    <h5 className={iconContTitle}>{skill.title}</h5>
+                    <p className='color-blue'>Planning to study</p>
+                  </div>
+                </>
+              )}
+            </div>
+          ))
+      )}
     </div>
   );
 };
